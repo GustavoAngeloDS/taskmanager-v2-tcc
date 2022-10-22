2ae25d8cf9d4823ebc55757ec16393dfddab20d2
Gustavo Angelo da Silva <gustavo.silvaangelo@gmail.com>
2022-10-22 15:56:12 -0300

    Html Body on email

--- src/main/java/com/api/taskmanager/constants/Helper.java
+++ src/main/java/com/api/taskmanager/constants/Helper.java
@@ -2,8 +2,14 @@ package com.api.taskmanager.constants;
 
 public final class Helper {
     public static final String EMAIL_SUBJECT = "Convite para o quadro %s";
-    public static final String EMAIL_BODY = "Você foi convidado a fazer parte do board %s na " +
-            "plataforma TaskManager.\nFaça parte clicando no link abaixo. \n%s";
+    public static final String EMAIL_BODY =
+            "<html>" +
+                "<body>" +
+                    "<h1>Você foi convidado a fazer parte do board %s na plataforma TaskManager</h1>" +
+                    "<p>Para aceitar o vinculo com o quadro, clique no botão abaixo.</p>" +
+                    "<a href=\"%s\"><button>Clique aqui</button></a>" +
+                "</body>" +
+            "</html>";
 
     private Helper() {}
 }
